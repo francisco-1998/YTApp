95f384c224630a475946acc813e1437f753fe643
francisco-1998 <francmarinc@gmail.com>
2020-10-19 11:52:40 -0500

    Creacion del servicio para consumir el API de YT

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { YoutubeResponse } from '../models/youtube.model';
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
 
-  constructor() { }
+  private ytUrl = 'https://www.googleapis.com/youtube/v3';
+  private apiKey = 'AIzaSyARevbBHxF8SExzJ50K8EVpXsQ-ZkOQTzM';
+  private playlist = 'UUyQqzYXQBUWgBTn4pw_fFSQ';
+  private nextPageToken = '';
+
+
+  constructor(private http: HttpClient) { }
+
+
+  getVideos() {
+
+    const url = `${this.ytUrl}/playlistItems`;
+    const params = new HttpParams()
+      .set('playlistId', this.playlist)
+      .set('part', 'snippet')
+      .set('key', this.apiKey)
+      .set('maxResults', '10')
+
+    return this.http.get<YoutubeResponse>(url, { params: params }).pipe(
+      map(resp => {
+        this.nextPageToken = resp.nextPageToken;
+        return resp.items;
+      }),
+      map(items => {
+        return items.map(video => {
+          return video.snippet;
+        })
+      })
+    )
+  }
+
 }
+
+
+// https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=AIzaSyARevbBHxF8SExzJ50K8EVpXsQ-ZkOQTzM&playlistId=UUyQqzYXQBUWgBTn4pw_fFSQ&maxResults=10
